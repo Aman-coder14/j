80eca66e4a1a17b1abe5facfbb60dae542e5e003
Aman Singh <amansingh81028@gmail.com>
2025-07-19 22:34:38 +0530

    conversion of datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "Aman"
+// console.log(typeof score);
+// console.log(typeof(score))
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
+
+
+// "33 "  => 33
+// "33abbc"=> Nan
+// true => 1; false =0;
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber) 
+console.log(typeof stringNumber)
+
